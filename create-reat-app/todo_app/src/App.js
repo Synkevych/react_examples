a9447e74b2285dd2ccb0f8e491cd4d87d8e88c87
Synkevych Roman <sunkevu4@gmail.com>
2019-09-03 12:19:57 +0300

    fix: add style, rename functions

--- create-reat-app/todo_app/src/App.js
+++ create-reat-app/todo_app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-const TextField = ({ text }) => {
+const TodoItem = ({ text }) => {
 	let style = {
-		fontStyle: "Arial"
+		fontStyle: 'Arial'
 	};
 	return <li style={style}>{text}</li>;
 };
@@ -11,42 +11,48 @@ const TextField = ({ text }) => {
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { inpuText: '', data: [] };
+		this.state = { inpuText: '', todos: [] };
 		this.handleClick = this.handleClick.bind(this);
 	}
 	handleClick(e) {
+		e.preventDefault();
+		const TODOS = [...this.state.todos, this.state.inpuText];
+		console.log(TODOS);
+		this.setState({ todos: TODOS, inpuText: '' });
+	}
+	/* My solution 
+	handleClick(e) {
+		e.preventDefault();
 		let text = this.state.inpuText.slice();
-		console.log('text', text);
 		this.setState(state => {
-			let newData = state.data.slice();
+			let newData = state.todos.slice();
 			newData.push(text);
-			return { data: newData, inpuText: '' };
+			return { todos: newData, inpuText: '' };
 		});
 	}
+	*/
 	render() {
-		let { inpuText, data } = this.state;
-		console.log('this.state', this.state);
-		const textField = data.map((text, index) => {
-			return <TextField key={index} text={text} />;
+		let { inpuText, todos } = this.state;
+console.log("this.state", this.state.todos);
+		const todo = todos.map((text, index) => {
+			return <TodoItem key={index} text={text} />;
 		});
 		return (
 			<div className='app'>
 				<h2>Simple ToDo App </h2>
-				<form
-					onSubmit={e => {
-						e.preventDefault();
-						this.handleClick(e);
-					}}
-				>
+				<form onSubmit={e => this.handleClick(e)}>
 					<input
+						type='text'
+						autoComplete='off'
+						name='inpuText'
 						value={inpuText}
 						onChange={e => {
-							this.setState({ inpuText: e.target.value });
+							this.setState({ [e.target.name]: e.target.value });
 						}}
 					/>
-					<button>SAVE</button>
+					<button type='submit'>SAVE</button>
 				</form>
-				<ol>{textField} </ol>
+				<ol>{todo} </ol>
 			</div>
 		);
 	}
