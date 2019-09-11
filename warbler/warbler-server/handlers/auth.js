e134f90d1377e2419ea8b400b97a6a714ff1d276
Synkevych Roman <sunkevu4@gmail.com>
2019-09-11 23:10:36 +0300

    feat: signin function

--- warbler/warbler-server/handlers/auth.js
+++ warbler/warbler-server/handlers/auth.js
@@ -1,7 +1,45 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signin = async function(req, res, next) {};
+exports.signin = async function(req, res, next) {
+	// finding a user
+	// checking if their password matches what was sent to the server
+	// if it all matches
+	// lo them
+	try {
+		let user = await db.User.findOne({
+			email: req.body.email
+		});
+		let { id, username, profileImageUrl } = user;
+		let isMatch = await user.comparePassword(req.body.password);
+		if (isMatch) {
+			let token = jwt.sign(
+				{
+					id,
+					username,
+					profileImageUrl
+				},
+				process.env.SECRET_KEY
+			);
+			return res.status(200).json({
+				id,
+				username,
+				profileImageUrl,
+				token
+			});
+		} else {
+			return next({
+				status: 400,
+				message: 'Invalid Email/Password.'
+			});
+		}
+	} catch (e) {
+		return next({
+			status: 400,
+			message: 'Invalid Email/Password.'
+		});
+	}
+};
 
 exports.signup = async function(req, res, next) {
 	try {
