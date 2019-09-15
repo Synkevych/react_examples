6e21d5c5381701941425d213a76e887d862a1d42
Synkevych Roman <sunkevu4@gmail.com>
2019-09-15 21:37:28 +0300

    fix: get data from server & token useg

--- warbler/warbler-client/src/components/Homepage.js
+++ warbler/warbler-client/src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Homepage.css";
 
 const Homepage = () => (
 	<div className='home-hero'>
--- warbler/warbler-client/src/containers/Main.js
+++ warbler/warbler-client/src/containers/Main.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Switch, Route, withRouter, Reirect } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Homepage from "../components/Homepage";
+import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
+import { authUser } from '../store/actions/auth';
+import { removeError } from '../store/actions/errors';
 
 const Main = props => {
+	const { authUser, currentUser, errors, removeError } = props;
 	return (
 		<div className='container'>
 			<Switch>
-				<Route exact path='/' render={props => <Homepage {...props} />} />
+				<Route
+					exact
+					path='/'
+					render={props => (
+						<Homepage errors={errors} currentUser={currentUser} {...props} />
+					)}
+				/>
+				<Route
+					exact
+					path='/signin'
+					render={props => {
+						return (
+							<AuthForm
+								removeError={removeError}
+								errors={errors}
+								onAuth={authUser}
+								buttonText='Log in'
+								heading='Welcome Back.'
+								{...props}
+							/>
+						);
+					}}
+				/>
+				<Route
+					exact
+					path='/signup'
+					render={props => {
+						return (
+							<AuthForm
+							removeError={removeError}
+								errors={errors}
+								onAuth={authUser}
+								signUp
+								buttonText='Sign me up'
+								heading='Join Wabler today.'
+								{...props}
+							/>
+						);
+					}}
+				/>
 			</Switch>
 		</div>
 	);
@@ -15,7 +58,14 @@ const Main = props => {
 
 function mapStateToProps(state) {
 	return {
-		currentUser: state.currentUser
+		currentUser: state.currentUser,
+		errors: state.errors
 	};
 }
-export default withRouter(connect(mapStateToProps, null)(Main));
+
+export default withRouter(
+	connect(
+		mapStateToProps,
+		{ authUser, removeError }
+	)(Main)
+);
